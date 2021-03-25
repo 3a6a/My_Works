4f22e5677a41897deceaa57053f289c79027c9cc
Andrey_Zabelin <3a6a86@gmail.com>
2021-03-25 16:37:55 +0200

    changes

--- carsharing/js/main.js
+++ carsharing/js/main.js
@@ -4,7 +4,20 @@ $('.top__slider').slick({
     arrows: false,
     dots: true,
     autoplay: true,
-    fade: true
+    fade: true,
+    responsive: [
+        {
+          breakpoint: 1200,
+          settings: {
+          
+            dots: false
+          }
+        
+        },
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
 });
 
 $('.reviews__slider').slick({
@@ -12,8 +25,35 @@ $('.reviews__slider').slick({
     dots: true,
     slidesToShow: 4,
     slidesToScroll: 1,
-    autoplay: true
+    autoplay: true,
+    responsive: [
+        {
+          breakpoint: 1141,
+          settings: {
+            slidesToShow: 3
+          }
+          },
+          {breakpoint: 846,
+          settings: {
+            slidesToShow: 2
+          }
+          },
+          {breakpoint: 585,
+          settings: {
+            slidesToShow: 1
+          }
+        
+        },
+
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
     
 })
+
+$('.menu__btn').on('click', function(){
+$('.menu__list').toggleClass('menu__list--active');
+});
 
 });
